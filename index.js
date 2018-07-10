fa912d2044c2a922af75c569073cdb28a256b85c
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-10 10:07:44 +0530

    Removed duplicate items using concept of Set and fixed linting issues

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
  * @param {Array} items
  * @returns {Array}
  */
-const removeDuplicateItems = (items) => {};
+const removeDuplicateItems = (items) => {
+  const mySet = new Set(items);
+  const myArray = [...mySet];
+  return myArray;
+};
 
 module.exports = removeDuplicateItems;
